8af26b6b322300c57009a18093092fb626ad776c
HoaNguyenThu12 <97510320006@ispace.edu.vn>
2024-06-12 23:32:17 +0700

    Initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+
+import Background from "./Background";
+import Content from "./Content";
+function Home() {
+    return (
+        <div>
+            <Background/>
+            <Content/>
+        </div>
+    )
+}
+export default Home;
